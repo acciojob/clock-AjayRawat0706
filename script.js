1f3e711e2d59bbb21dd994328c1466f2df1c6d65
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-19 18:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+setInterval(() => {
+	let date = new Date();
+	let elem = document.getElementById("timer");
+	elem.innerText = date.toLocaleString("en-US");
+}, 1000);
